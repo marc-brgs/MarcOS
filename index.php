3322d08140cec7953e581b6f9c93fe1cfa643a72
Marc Brgs <66321703+marc-brgs@users.noreply.github.com>
2022-07-30 20:31:41 +0200

    New windows linked with icons and taskbar
    - Differents windows with their own content
    - Window buttons working (minimize, maximize and close)
    - Windows are linked with an icon and a task in taskbar for good window behaviour
    - Double click on icon behaviour handled
    - Click on wallpaper behaviour handled
    - Click on task in taskbar behaviour handled
    - Icons and tasks :active design changed

--- index.php
+++ index.php
@@ -1,35 +1,44 @@
+<?php
+$window_offset = 50;
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>MarcOS</title>
 		<meta charset="UTF-8">
+		<link rel="icon" type="image/png" href="img/windows_2000_logo.png">
 		<?php
 		$time = time();
 		echo "
-		<link rel='stylesheet' href='css/index.css?i={$time}'>
+		<link rel='stylesheet' href='css/desktop.css?i={$time}'>
+		<link rel='stylesheet' href='css/window.css?i={$time}'>
 		<script src='js/index.js?i={$time}'></script>";
 		?>
 	</head>
 	<body>
 		<div class="wallpaper">
+			<span class="sign">MarcOS</span>
 			<div class="grid">
-				<div class="icon">
-					<img src="img/my_documents.png"></img>
-					<span>My Documents</span>
-				</div>
-				<div class="icon">
-					<img src="img/my_computer.png"></img>
-					<span>My Computer</span>
-				</div>
-				<div class="icon">
-					<img src="img/network_places.png"></img>
-					<span>My Network Places</span>
-				</div>
-				<div class="icon">
-					<img src="img/recycle_bin_empty.png"></img>
-					<span>Recycle Bin</span>
-				</div>
+				<?php
+				drawIcon("01", "img/my_documents.png", "My Documents");
+				drawIcon("02", "img/task_manager.png", "Task Manager");
+				drawIcon("03", "img/my_pictures.png", "My Pictures");
+				drawIcon("04", "img/sticpl_cpl.png", "Camera");
+				drawIcon("05", "img/my_computer.png", "My Computer");
+				drawIcon("06", "img/network_places.png", "My Network Places");
+				drawIcon("09", "img/recycle_bin_empty.png", "Recycle Bin");
+				?>
 			</div>
+			
+			<?php
+			drawWindow("01", "img/my_documents.png", "My Documents", "frames/Ice-Cream-Configurator");
+			drawWindow("02", "img/task_manager.png", "Task Manager", "frames/Jeu-Juste-Prix-PHP");
+			drawWindow("03", "img/my_pictures.png", "My Pictures", "frames/Site-Web-Vente-De-Fleurs-HTML-CSS");
+			drawWindow("04", "img/sticpl_cpl.png", "Camera", "https://www.google.com/search?igu=1&q=marcos%20le%20crack");
+			drawWindow("09", "img/recycle_bin_empty.png", "Recycle Bin", "");
+			?>
+			
 			<footer class="taskbar">
 				<div class="start-menu">
 					<img src="img/windows_2000_logo.png"></img>
@@ -38,22 +47,12 @@
 				<div class="task-separator"></div>
 				<div class="container">
 					<ul class="tasks">
-						<li class="task">
-							<img src="img/my_documents.png"></img>
-							<span>My Documents</span>
-						</li>
-						<li class="task">
-							<img src="img/task_manager.png"></img>
-							<span>Task Manager</span>
-						</li>
-						<li class="task">
-							<img src="img/my_pictures.png"></img>
-							<span>Nudes</span>
-						</li>
-						<li class="task">
-							<img src="img/sticpl_cpl.png"></img>
-							<span>Pornhub</span>
-						</li>
+						<?php
+						drawTask("01", "img/my_documents.png", "My Documents");
+						drawTask("02", "img/task_manager.png", "Task Manager");
+						drawTask("03", "img/my_pictures.png", "My Pictures");
+						drawTask("04", "img/sticpl_cpl.png", "Camera");
+						?>
 					</ul>
 				</div>
 			</footer>
@@ -61,3 +60,40 @@
 		</div>
 	</body>
 </html>
+
+<?php
+function drawIcon($pid, $img, $name) {
+	echo "<div class='icon' pid='{$pid}'>
+		<img src='{$img}'></img>
+		<span>{$name}</span>
+	</div>";
+}
+
+function drawWindow($pid, $img, $name, $frame) {
+	global $window_offset;
+	echo "<div class='window' style='display: none; top: {$window_offset}px; left: {$window_offset}px;' pid='{$pid}'>
+				<div class='window-header'>
+					<div class='window-title-container'>
+						<img src='{$img}'></img>
+						<span class='window-name'>{$name}</span>
+					</div>
+					<div class='window-btn-container'>
+						<div class='window-btn minimize'></div>
+						<div class='window-btn maximize'></div>
+						<div class='window-btn close'></div>
+					</div>
+				</div>
+				<div class='window-content'>
+					<iframe src='{$frame}' style='width: 100%; height: 100%;'></iframe>
+				</div>
+			</div>";
+	$window_offset += 50;
+}
+
+function drawTask($pid, $img, $name) {
+	echo "<li class='task' pid='{$pid}'>
+		<img src='{$img}'></img>
+		<span>{$name}</span>
+	</li>";
+}
+?>
--- js/index.js
+++ js/index.js
@@ -2,62 +2,305 @@
   Creation : 29/07/2022
   Author : Marc BOURGEOIS
  */
+ 
 window.onload = init;
 
 function init() {
-	const body = document.body;
+	console.log("MarcOS successfully loaded");
+	
+	// First set
+	const windows = document.getElementsByClassName("window");
+	for(let wdw of windows) {
+		wdw.style.width = window.innerWidth*(1/2) + "px";
+		wdw.style.height = window.innerHeight*(2/3) + "px";
+		wdw.addEventListener('click', event => {awakeWindow(event.target)});
+	}
 	
 	// Add listeners
 	
-	let wallpaper = document.getElementsByClassName("wallpaper")[0];
-	wallpaper.addEventListener('click', event => {
-		if(event.target.className !== "wallpaper" && event.target.className !== "grid") return; // from wallpaper
-		deselectAll()});
-	
-	let icons = document.getElementsByClassName("icon");
-	let l = icons.length;
-	for(let i = 0; i < l; i++) {
-		icons[i].addEventListener('click', event => {activeIcon(event.target)});
+	// Correctly handle iframes when dragging or resizing window
+	const body = document.body;
+	body.addEventListener('mousedown', () => {
+		let iframes = document.getElementsByTagName("IFRAME");
+		for(let iframe of iframes) {
+			iframe.style.pointerEvents = "none";
+		}
+		});
+	body.addEventListener('mouseup', () => {
+		let iframes = document.getElementsByTagName("IFRAME");
+		for(let iframe of iframes) {
+			iframe.style.pointerEvents = "auto";
+		}
+		});
+		
+	// Iframe awake window (not working)
+	let iframes = document.getElementsByTagName("IFRAME");
+	for(let iframe of iframes) {
+		// iframe.contentWindow.document.body.addEventListener('mousedown', (event, iframe) => {
+			// awakeWindow(iframe);
+		// });
 	}
 	
+	// Click on desktop
+	let wallpaper = document.getElementsByClassName("wallpaper")[0];
+	wallpaper.addEventListener('click', event => {clickOnWallpaper(event.target)});
+	
+	// Click on icon
+	const icons = document.getElementsByClassName("icon");
+	for(let icon of icons) {
+		icon.addEventListener('click', event => {clickOnIcon(event.target)});
+	}
+	
+	// Click on task in taskbar
 	let tasks = document.getElementsByClassName("task");
-	l = tasks.length;
-	for(let i = 0; i < l; i++) {
-		tasks[i].addEventListener('click', event => {activeTask(event.target)});
+	for(let task of tasks) {
+		task.addEventListener('click', event => {clickOnTask(event.target)});
+	}
+	
+	// Draggable and onclick windows
+	for(let wdw of windows) {
+		dragWindow(wdw);
+		wdw.addEventListener('click', event => {awakeWindow(event.target)});
+	}
+	
+	// Window buttons
+	const btnMinimize = document.getElementsByClassName("window-btn minimize");
+	for(let btn of btnMinimize) {
+		btn.addEventListener('click', event => {windowBtnMinimize(event)});
+	}
+	const btnMaximize = document.getElementsByClassName("window-btn maximize");
+	for(let btn of btnMaximize) {
+		btn.addEventListener('click', event => {windowBtnMaximize(event)});
+	}
+	const btnClose = document.getElementsByClassName("window-btn close");
+	for(let btn of btnClose) {
+		btn.addEventListener('click', event => {windowBtnClose(event)});
 	}
 	
 }
 
-function deselectAll() {	
+function deselectAll() {
 	// Icons
 	let icons = document.getElementsByClassName("icon");
-	
-	let l = icons.length;
-	for(let i = 0; i < l; i++) {
-		icons[i].removeAttribute("focused");
+	for(let icon of icons) {
+		icon.removeAttribute("focused");
 	}
 	
 	// Tasks
 	let tasks = document.getElementsByClassName("task");
+	for(let task of tasks) {
+		task.removeAttribute("focused");
+	}
 	
-	l = tasks.length;
-	for(let i = 0; i < l; i++) {
-		tasks[i].removeAttribute("focused");
+	// Windows
+	let windows = document.getElementsByClassName("window");
+	for(let wdw of windows) {
+		wdw.setAttribute('idle', '');
 	}
 }
 
-function activeTask(task) {
+function clickOnWallpaper(target) {
+	if(target.className !== "wallpaper" && target.className !== "grid") return; // from wallpaper
+	
+	deselectAll();
+}
+
+function clickOnTask(task) {
 	while(task.className !== "task") task = task.parentNode; // from child redirect to parent
 	
+	const pid = task.getAttribute("pid");
+	let pid_wdw = null;
+	
+	const windows = document.getElementsByClassName("window");
+	for(let wdw of windows) {
+		if(wdw.getAttribute("pid") === pid) {
+			pid_wdw = wdw;
+			break;
+		}
+	}
+	
+	if(!pid_wdw) return;
+	
+	// Reduce window if already focused
+	if(task.getAttribute("focused") !== null) {
+		deselectAll();
+		pid_wdw.style.display = "none";
+		pid_wdw.setAttribute('idle', '');
+		return;
+	}
+	
+	// Focus window
 	deselectAll();
 	
 	task.setAttribute("focused", "");
+	focusWindow(pid_wdw);
 }
 
-function activeIcon(icon) {
+function focusWindow(wdw) {
+	// Idle all other windows
+	let windows = document.getElementsByClassName("window");
+	for(let w of windows) {
+		w.setAttribute('idle', '');
+		w.style.zIndex = 10;
+	}
+	wdw.removeAttribute('idle');
+	wdw.style.zIndex = 11;
+	
+	// Active task
+	const pid = wdw.getAttribute("pid");
+	let tasks = document.getElementsByClassName("task");
+	for(let task of tasks) {
+		task.removeAttribute("focused");
+		if(task.getAttribute("pid") === pid) {
+			task.setAttribute("focused", "");
+		}
+	}
+	
+	// Show and set to foreground
+	wdw.style.display = null;
+}
+
+function clickOnIcon(icon) {
 	while(icon.className !== "icon") icon = icon.parentNode; // from child redirect to parent
 	
+	// Double click
+	if(icon.getAttribute("focused") !== null) {
+		doubleClickOnIcon(icon);
+		return;
+	}
 	deselectAll();
 	
 	icon.setAttribute("focused", "");
+}
+
+function doubleClickOnIcon(icon) {
+	const pid = icon.getAttribute("pid");
+	
+	let tasks = document.getElementsByClassName("task");
+	for(let task of tasks) {
+		if(task.getAttribute("pid") === pid) {
+			task.style.display = null;
+			task.setAttribute("focused", "");
+			break;
+		}
+	}
+	
+	let windows = document.getElementsByClassName("window");
+	for(let wdw of windows) {
+		if(wdw.getAttribute("pid") === pid) {
+			focusWindow(wdw);
+			break;
+		}
+	}
+}
+
+function awakeWindow(wdw) {
+	while(wdw.className !== "window") wdw = wdw.parentNode; // from child redirect to parent
+	focusWindow(wdw);
+}
+
+function dragWindow(wdw) {
+	var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+	
+	if (wdw.getElementsByClassName("window-header")) {
+		wdw.getElementsByClassName("window-header")[0].onmousedown = dragMouseDown;
+	} else {
+		wdw.onmousedown = dragMouseDown;
+	}
+
+	function dragMouseDown(e) {
+		// Cancel if btn click
+		if(e.target.classList.contains("window-btn")) return;
+		
+		// Focus wdw
+		let wdw = e.target;
+		while(wdw.className !== "window") wdw = wdw.parentNode;
+		focusWindow(wdw);
+		
+		e = e || window.event;
+		e.preventDefault();
+		
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		document.onmouseup = closeDragElement;
+    
+		document.onmousemove = elementDrag;
+	}
+
+	function elementDrag(e) {
+		e = e || window.event;
+		e.preventDefault();
+    
+		pos1 = pos3 - e.clientX;
+		pos2 = pos4 - e.clientY;
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		
+		wdw.style.top = (wdw.offsetTop - pos2) + "px";
+		wdw.style.left = (wdw.offsetLeft - pos1) + "px";
+	}
+
+	function closeDragElement() {
+		document.onmouseup = null;
+		document.onmousemove = null;
+		
+		// Anti window hididng
+		if(wdw.getBoundingClientRect().top < 0)
+			wdw.style.top = "0px";
+		else if(wdw.getBoundingClientRect().top > window.innerHeight-40) {
+			wdw.style.top = window.innerHeight-50 + "px";
+		}
+		if(wdw.getBoundingClientRect().left > window.innerWidth-20)
+			wdw.style.left = window.innerWidth-50 + "px";
+		else if(wdw.getBoundingClientRect().right < 200)
+			wdw.style.left = wdw.getBoundingClientRect().left+(200-wdw.getBoundingClientRect().right) + "px";
+	}
+}
+
+function windowBtnMinimize(e) {
+	e.stopPropagation();
+	
+	// Recover window
+	let target = e.target;
+	while(target.className !== "window") target = target.parentNode;
+	
+	deselectAll();
+	target.style.display = "none";
+	target.setAttribute('idle', '');
+}
+
+function windowBtnMaximize(e) {
+	e.stopPropagation();
+	
+	// Recover window
+	let target = e.target;
+	while(target.className !== "window") target = target.parentNode;
+	
+	if(target.getAttribute("maximized") !== null)
+		target.removeAttribute("maximized");
+	else
+		target.setAttribute("maximized", "");
+}
+
+function windowBtnClose(e) {
+	e.stopPropagation();
+	
+	// Recover window
+	let target = e.target;
+	while(target.className !== "window") target = target.parentNode;
+	
+	const pid = target.getAttribute("pid");
+	
+	deselectAll();
+	target.style.display = "none";
+	target.setAttribute('idle', '');
+	target.removeAttribute("maximized");
+	
+	let tasks = document.getElementsByClassName("task");
+	for(let task of tasks) {
+		if(task.getAttribute("pid") === pid) {
+			task.style.display = "none";
+			break;
+		}
+	}
 }
